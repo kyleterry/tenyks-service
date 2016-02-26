9d5ce4e34d902bb4272b6b726a3c537639983c72
Kyle Terry <kyle@kyleterry.com>
2016-02-25 22:31:08 -0800

    cleaning up setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,41 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-version = '1.8'
+version = '1.9'
 
 packages = [
     'tenyksservice',
 ]
 
+with open('./README.md') as f:
+    long_description = f.read()
+
 setup(name='tenyksservice',
-    version=version,
-    description="Tenyks service class for making fun things",
-    long_description="""\
-""",
-    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords='irc bot redis service',
-    author='Kyle Terry',
-    author_email='kyle@kyleterry.com',
-    url='https://github.com/kyleterry/tenyks-service',
-    license='LICENSE',
-    packages=packages,
-    package_dir={'tenyksservice': 'tenyksservice'},
-    package_data={'tenyksservice': ['settings.py.dist']},
-    include_package_data=True,
-    zip_safe=False,
-    test_suite='tests',
-    install_requires=[
-        'gevent',
-        'redis',
-        'nose',
-        'unittest2',
-        'jinja2',
-    ],
-    entry_points={
-        'console_scripts': [
-            'tenyks-service-mkconfig = tenyksservice.config:make_config'
-        ]
-    },
-)
+      version=version,
+      description="Tenyks service class for making fun things",
+      long_description=long_description,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='irc bot redis service',
+      author='Kyle Terry',
+      author_email='kyle@kyleterry.com',
+      url='https://github.com/kyleterry/tenyks-service',
+      license='LICENSE',
+      packages=packages,
+      package_dir={'tenyksservice': 'tenyksservice'},
+      package_data={'tenyksservice': ['settings.py.dist']},
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='tests',
+      install_requires=[
+          'gevent',
+          'redis',
+          'nose',
+          'unittest2',
+          'jinja2',
+      ],
+      entry_points={
+          'console_scripts': [
+              'tenyks-service-mkconfig = tenyksservice.config:make_config'
+          ]
+      },
+      )
